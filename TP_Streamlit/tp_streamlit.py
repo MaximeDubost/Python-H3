e34048eaebb231459f239780c2c47e03f289b8ee
Maxime Dubost <maxime.dubost.75@gmail.com>
2020-11-17 17:00:13 +0100

    TP Streamlit du 17/11/2020 (après-midi)

--- TP_Streamlit/tp_streamlit.py
+++ TP_Streamlit/tp_streamlit.py
@@ -1,84 +1,124 @@
+# Imports
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import os
+from streamlit.errors import Error
 
-st.write("##### Maxime DUBOST")
-st.write("##### M1 IoT")
-st.write("##### 17/11/2020")
+# Strings
+str_datapath = "D:/Dataset"
+str_student_name = "##### Maxime DUBOST"
+str_student_group = "##### M1 IoT"
+str_work_date = "##### 17/11/2020"
+str_title = "Visualisation interactive des données"
+str_menu = "Menu"
+str_datainfo = "Description"
+str_dataviz = "Visualisation"
+str_select_dataset = "Sélectionnez un dataset"
+str_none_dataset = "Aucun dataset sélectionné"
+str_error = "Impossible d'afficher les données"
+str_longitude = "longitude"
+str_latitude = "latitude"
+str_lon = "lon"
+str_lat = "lat"
+str_divider = "---"
 
-st.title("TP : réalisation d'une data app ")
-st.write("Votre mission est de construire une petite application de visualisation interactive de données avec l’outil Streamlit vu au chapitre précédent, qui contiendra les fonctionnalités suivantes :")
 
-# ----------------------------------------------------------------
+dataset_list = os.listdir(str_datapath)
+selected_menu = st.sidebar.radio(str_menu, [str_datainfo, str_dataviz])
 
-st.write("## - Charger des jeux de données (au moins 2) présents dans votre répertoire local")
+st.write(str_student_name)
+st.write(str_student_group)
+st.write(str_work_date)
 
-st.write("**Dataset 1 :** Localisations des pays")
-st.write("**Dataset 2 :** Localisations des états des USA")
-st.write("Lien vers le dataset (2 en 1) : *https://www.kaggle.com/paultimothymooney/latitude-and-longitude-for-every-country-and-state/version/1*")
+st.title(str_title)
+selected_dataset = st.selectbox(str(), [str_select_dataset] + dataset_list)
+st.write(str_divider)
 
-df1 = pd.read_csv('C:/Users/Maxime/Python/TP_Streamlit/world_country_and_usa_states_latitude_and_longitude_values.csv')[['country_code', 'latitude', 'longitude', 'country']].dropna()
-df2 = pd.read_csv('C:/Users/Maxime/Python/TP_Streamlit/world_country_and_usa_states_latitude_and_longitude_values.csv')[['usa_state_code', 'usa_state_latitude', 'usa_state_longitude', 'usa_state']].rename(columns={'usa_state_latitude': 'latitude', 'usa_state_longitude': 'longitude'}).dropna()
+if selected_menu == str_datainfo:
+    st.write("## **" + str_datainfo + "**") 
+if selected_menu == str_dataviz:
+    st.write("## **" + str_dataviz + "**") 
 
-# ----------------------------------------------------------------
 
-st.write("## - Afficher le dataset chargé suivant un nombre de ligne entrées par l’utilisateur")
+try:
 
-st.write("### **Dataset 1 :**")
-df1_count = st.slider('Nombre d\'éléments à afficher :', max_value=len(df1))
-st.write(df1.head(df1_count))
 
-st.write("### **Dataset 2 :**")
-df2_count = st.slider('Nombre d\'éléments à afficher :', max_value=len(df2))
-st.write(df2.head(df2_count))
+    if selected_dataset == str_select_dataset:
+        st.warning(str_none_dataset)
 
-# ----------------------------------------------------------------
 
-st.write("## - Afficher le nom des colonnes du dataset")
+    if selected_dataset != str_select_dataset:
+        df = pd.read_csv("D:/Dataset/" + selected_dataset)
 
-st.write("### **Dataset 1 :**")
-st.write("Nom des colonnes", df1.columns)
 
-st.write("### **Dataset 2 :**")
-st.write("Nom des colonnes", df2.columns)
+        # Description
+        if selected_menu == str_datainfo:
 
-# ----------------------------------------------------------------
 
-st.write("## - Afficher le type des colonnes du dataset ainsi que les colonnes sélectionnées")
+            # Données
+            st.write("## Données")
+            try:
+                line_count = st.number_input("Sélectionnez le nombre de lignes à afficher :", value=5, min_value=1, max_value=len(df))
+                st.write(df.head(line_count))
+            except:
+                st.error(str_error)
 
-st.write("### **Dataset 1 :**")
-st.write("Types de colonnes : ", df1.dtypes)
 
-st.write("### **Dataset 2 :**")
-st.write("Types de colonnes : ", df2.dtypes)
+            # Colonnes
+            st.write("## Colonnes")
+            try:
+                st.write(df.columns)
+            except:
+                st.error(str_error)
 
-# ----------------------------------------------------------------
 
-st.write("## - La shape du dataset, par lignes et par colonnes")
+            # Types des colonnes
+            st.write("## Types des colonnes")
+            try:
+                selected_column = st.selectbox("Sélectionnez la colonne dont vous souhaitez afficher le type :", df.columns)
+                st.write("La colonne **", selected_column, "** est de type ", df[selected_column].dtype)
+            except:
+                st.error(str_error)
 
-st.write("### **Dataset 1 :**")
-st.write("Nombre de lignes : ", str(df1.shape[0]))
-st.write("Nombre de colonnes : ", str(df1.shape[1]))
 
-st.write("### **Dataset 2 :**")
-st.write("Nombre de lignes : ", str(df2.shape[0]))
-st.write("Nombre de colonnes : ", str(df2.shape[1]))
+            # Forme
+            st.write("## Forme")
+            try:
+                st.write("Ce jeu de données contient **" + str(df.shape[0]) + "** ligne(s) et **" + str(df.shape[1]) + "** colonne(s)")
+            except:
+                st.error(str_error)
 
-# ----------------------------------------------------------------
 
-st.write("## - Afficher les statistiques descriptives du dataset")
+            # Statistiques descriptives
+            st.write("## Statistiques descriptives")
+            try:
+                st.write(df.describe())
+            except:
+                st.error(str_error)
 
-st.write("### **Dataset 1 :**")
-st.write(df1.describe())
 
-st.write("### **Dataset 2 :**")
-st.write(df2.describe())
+        # Visualisation
+        if selected_menu == str_dataviz:
+            
 
-# ----------------------------------------------------------------
+            # Heatmap
+            st.write("## Heatmap")
+            try:
+                st.write(sns.heatmap(df.corr(), annot=True))
+                st.pyplot()
+            except:
+                st.error(str_error)
 
-st.write("## - Affichage d'une map :")
 
-st.write("### **Dataset 1 :**")
-st.map(df1)
+            # Carte
+            if (str_longitude in df.columns or str_lon in df.columns) and (str_latitude in df.columns or str_lat in df.columns):
+                st.write("## Carte")
+                try:
+                    st.map(df)
+                except:
+                    st.error(str_error)
 
-st.write("### **Dataset 2 :**")
-st.map(df2)
+
+except:
+    st.error(str_error)

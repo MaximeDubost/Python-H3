822d625baf122d5eb5b885f653eae1f31dc45eb6
Maxime Dubost <maxime.dubost.75@gmail.com>
2020-11-30 12:34:14 +0100

    TP Flask du 30/11/2020 (matin)

---
+++ MiniBookAPI/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+book=[
+	{
+		'id': 1,
+		'title': 'un titre',
+	},
+	{
+		'id': 2,
+		'title': 'un autre titre random',
+	}
+]
+
+# Page d'accueil
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+# Retourne tous les livres
+@app.route("/books", methods=["GET"])
+def get_books():
+    pass
+
+# Retourne un livre selon son id
+@app.route("/book/<int:id>", methods=["GET"])
+def get_book_by_id():
+    pass
+
+# Retourne un livre selon son titre
+@app.route("/book/<title>", methods=["GET"])
+def get_book_by_title():
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
